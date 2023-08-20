c5af3054ca2c5b8b0f78de76b693cdfc99443ec4
many bear <98581612+6666dustry@users.noreply.github.com>
2023-08-20 22:17:24 +0900

    add new packages for github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	base: "/color-dice/",
 	plugins: [sveltekit()],
 	assetsInclude: [
 		"**/*.csv"
